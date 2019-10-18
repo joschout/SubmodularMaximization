b8d5e3f69cf84d547b0c37c31b3b564c405cdcba
Jonas Schouterden <jonas.schouterden@student.kuleuven.be>
2019-10-18 14:07:35 +0200

    changed setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
 setup(
-    name='SubmodularMaximization',
-    version='',
+    name='Unconstrained Submodular Maximization',
+    version='0.0.1',
     packages=[''],
-    package_dir={'': 'src'},
-    url='',
-    license='',
+    package_dir={'': 'usm'},
+    url='https://github.com/joschout/SubmodularMaximization',
+    license='Apache 2.0',
     author='Jonas Schouterden',
     author_email='',
     description='Unconstrained submodular maximization optimization algorithms',
+    long_description=long_description,
     install_requires=['numpy']
 )
