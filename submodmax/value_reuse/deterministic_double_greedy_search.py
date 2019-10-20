69842f8206dc2d894080608e5a2c7f01d53869b4
joschout <jonasschouterden@gmail.com>
2019-10-20 17:44:09 +0200

    started value reuse for double greedy

---
+++ submodmax/value_reuse/deterministic_double_greedy_search.py
@@ -0,0 +1,109 @@
+from typing import Set, TypeVar, Tuple
+
+from value_reuse.abstract_optimizer import AbstractSubmodularFunctionValueReuse, AbstractOptimizerValueReuse, FuncInfo
+from value_reuse.set_info import SetInfo
+
+E = TypeVar('E')
+
+
+class DeterministicDoubleGreedySearch(AbstractOptimizerValueReuse):
+    """
+    Deterministic (Double Greedy) Unconstrained submodular maximization, by Buchbinder and Feldman
+
+    The approximation ration of this algorithm is 1/3.
+
+    There are two options:
+        either we add the current element to X_prev
+        or we remove it from Y_prev
+    The decision is done greedily based on the marginal gains/profits of each of the two options.
+
+    See also:
+        Buchbinder, N., Feldman, M., Naor, J. S., & Schwartz, R. (2015).
+        A tight linear time (1/2)-approximation for unconstrained submodular maximization.
+        SIAM Journal on Computing, 44(5), 1384–1402. https://doi.org/10.1137/130929205
+
+    """
+
+    def __init__(self, objective_function: AbstractSubmodularFunctionValueReuse, ground_set: Set[E],
+                 debug: bool = True):
+        super().__init__(objective_function, ground_set, debug)
+
+    def optimize(self) -> Tuple[SetInfo, FuncInfo]:
+        # X_prev: Set[E] = set()
+        # Y_prev: Set[E] = self.ground_set.copy(
+
+        ground_set_size = len(self.ground_set)
+        empty_set = set()
+
+
+        X_prev_set_info: SetInfo = SetInfo(
+            ground_set_size=ground_set_size,
+            current_set_size=0,
+            added_elems=,
+            deleted_elems=,
+            intersection_previous_and_current_elems=
+        )
+        X_prev_set_info.set_current_set(set())
+        
+        Y_prev_set_info = SetInfo(
+            ground_set_size=ground_set_size,
+            current_set_size=ground_set_size,
+            added_elems=,
+            deleted_elems=,
+            intersection_previous_and_current_elems=
+        )
+        Y_prev_set_info.set_current_set(self.ground_set.copy())
+        
+
+        func_info_X_prev: FuncInfo = self.objective_function.evaluate(X_prev_set_info, previous_func_info=None)
+        func_info_Y_prev: FuncInfo = self.objective_function.evaluate(Y_prev_set_info, previous_func_info=None)
+
+        elem: E
+        for elem in self.ground_set:
+
+            singleton_set = {elem}
+            
+            # X_prev_plus_elem: Set[E] = X_prev | {elem}
+            X_prev_plus_elem_set_info = SetInfo(
+                ground_set_size=ground_set_size,
+                current_set_size=X_prev_set_info.current_set_size + 1,
+                added_elems=singleton_set,
+                deleted_elems=empty_set,
+                intersection_previous_and_current_elems= X_prev_set_info.current_set
+            )
+
+            func_info_X_prev_plus_elem: FuncInfo = self.objective_function.evaluate(
+                X_prev_plus_elem_set_info, func_info_X_prev)
+            a: float = func_info_X_prev_plus_elem.func_value - func_info_X_prev.func_value
+
+            Y_prev_minus_elem_set: Set[E] = Y_prev_set_info.current_set - {elem}
+            Y_prev_minus_elem_set_info = SetInfo(
+                ground_set_size=ground_set_size,
+                current_set_size=Y_prev_set_info.current_set_size - 1,
+                added_elems=empty_set,
+                deleted_elems=singleton_set,
+                intersection_previous_and_current_elems=Y_prev_minus_elem_set
+            )
+            func_info_Y_prev_minus_elem = self.objective_function.evaluate(
+                Y_prev_minus_elem_set_info, func_info_Y_prev)
+            b: float = func_info_Y_prev_minus_elem.func_value - func_info_Y_prev.func_value
+
+            if a >= b:
+
+                new_set = X_prev_set_info.current_set | singleton_set
+                X_prev_plus_elem_set_info.set_current_set(new_set)
+                X_prev_set_info = X_prev_plus_elem_set_info
+
+                func_info_X_prev = func_info_X_prev_plus_elem
+                # Y_prev stays the same
+            else:
+                # X_prev stays the same
+                Y_prev = Y_prev_minus_elem_set_info
+                func_info_Y_prev = func_info_Y_prev_minus_elem
+
+        if not X_prev_set_info.current_set == Y_prev_set_info.current_set:
+            raise Exception("both sets should be equal")
+
+        if self.debug:
+            print("obj val local optimum:", str(func_info_X_prev.func_value))
+        return X_prev_set_info, func_info_X_prev
---
+++ submodmax/value_reuse/randomized_double_greedy_search.py
@@ -0,0 +1,80 @@
+import warnings
+from typing import Set, TypeVar
+
+import numpy as np
+
+from .abstract_optimizer import AbstractOptimizer, AbstractSubmodularFunction
+
+E = TypeVar('E')
+
+
+class RandomizedDoubleGreedySearch(AbstractOptimizer):
+    """
+    Randomized (Double Greedy) Unconstrained submodular maximization, by Buchbinder and Feldman
+
+    The approximation ratio of this algorithm is 1/2.
+
+    There are two options:
+        either we add the current element to X_prev
+        or we remove it from Y_prev
+    A 'smooth' decision is taken greedily based on values a and b.
+
+    IF both a and b are non-negative,
+    it randomly chooses whether to include or exclude the current element,
+        with a probability proportional to the ratio between a an b
+
+    See also:
+        Buchbinder, N., Feldman, M., Naor, J. S., & Schwartz, R. (2015).
+        A tight linear time (1/2)-approximation for unconstrained submodular maximization.
+        SIAM Journal on Computing, 44(5), 1384–1402. https://doi.org/10.1137/130929205
+
+    """
+
+    def __init__(self, objective_function: AbstractSubmodularFunction, ground_set: Set[E], debug: bool = True):
+        super().__init__(objective_function, ground_set, debug)
+
+    def optimize(self) -> Set[E]:
+        X_prev: Set[E] = set()
+        Y_prev: Set[E] = self.ground_set.copy()
+
+        f_on_X_prev: float = self.objective_function.evaluate(X_prev)
+        f_on_Y_prev: float = self.objective_function.evaluate(Y_prev)
+
+        elem: E
+        for elem in self.ground_set:
+            X_prev_plus_elem: Set[E] = X_prev | {elem}
+            f_on_X_prev_plus_elem: float = self.objective_function.evaluate(X_prev_plus_elem)
+            a: float = f_on_X_prev_plus_elem - f_on_X_prev
+
+            Y_prev_minus_elem: Set[E] = Y_prev - {elem}
+            f_on_Y_prev_minus_elem: float = self.objective_function.evaluate(Y_prev_minus_elem)
+            b: float = f_on_Y_prev_minus_elem - f_on_Y_prev
+
+            a_prime: float = max(a, 0)
+            b_prime: float = max(b, 0)
+
+            prob_boundary: float
+            if a_prime == 0 and b_prime == 0:
+                prob_boundary = 1
+            else:
+                prob_boundary = a_prime / (a_prime + b_prime)
+
+            random_value: float = np.random.uniform()
+
+            if random_value <= prob_boundary:
+                X_prev = X_prev_plus_elem
+                f_on_X_prev = f_on_X_prev_plus_elem
+                # Y_prev stays the same
+            else:
+                # X_prev stays the same
+                Y_prev = Y_prev_minus_elem
+                f_on_Y_prev = f_on_Y_prev_minus_elem
+
+        warnings.warn("remove equality check")
+
+        if not X_prev == Y_prev:
+            raise Exception("both sets should be equal")
+
+        if self.debug:
+            print("obj val local optimum:", str(f_on_X_prev))
+        return X_prev
